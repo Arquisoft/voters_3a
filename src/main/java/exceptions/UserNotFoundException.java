f12026033f76ef9524ea9a2dfef7b2b0c79de42c
davidglezz <davidglezz@outlook.es>
2016-02-15 14:35:46 +0100

    Implementacion de respuesta 404 2

---
+++ src/main/java/exceptions/UserNotFoundException.java
@@ -0,0 +1,19 @@
+package exceptions;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import types.UserPass;
+
+import org.springframework.http.HttpStatus;
+
+
+// No usado actualmente
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public UserNotFoundException(UserPass userPass) {
+		super("Usuario '" + userPass.getLogin() + "' no encontrado.");
+	}
+}
