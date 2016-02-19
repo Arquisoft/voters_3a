d98a54f34a921f2ce902c27519b487324147723a
Carlos <onecarlostlol@gmail.com>
2016-02-19 23:07:32 +0100

    Update MainController.java

--- src/main/java/voterAccess/MainController.java
+++ src/main/java/voterAccess/MainController.java
@@ -11,9 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import types.ChangePass;
-import types.UserInfo;
-import types.UserPass;
+import types.*;
 import exceptions.*;
 
 @Controller
