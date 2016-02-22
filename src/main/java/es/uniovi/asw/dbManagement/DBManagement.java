939cbc21fcd134c5299de2c134b428c1fdd036ea
davidglezz <davidglezz@outlook.es>
2016-02-22 01:29:59 +0100

    Fallo persistencia

--- src/main/java/es/uniovi/asw/dbManagement/DBManagement.java
+++ src/main/java/es/uniovi/asw/dbManagement/DBManagement.java
@@ -1,12 +1,9 @@
 package es.uniovi.asw.dbManagement;
 
-import org.springframework.stereotype.Component;
-
 import es.uniovi.asw.model.Voter;
 import es.uniovi.asw.types.ChangePass;
 import es.uniovi.asw.types.UserPass;
 
-@Component
 public interface DBManagement {
 
 	public Voter save(Voter voter);
